c316eaa8261139d585abd458f8c318970c7ab437
Omr50 <omrab4@gmail.com>
2026-06-28 16:55:34 -0400

    added parse array

--- RESP.py
+++ RESP.py
@@ -1,6 +1,19 @@
 # Common values
 CRLF = '\r\n'
 
+def parse(input):
+    identifier = input[0]
+    if identifier == '+':
+        return parseString(input)
+    if identifier == '-':
+        return parseError(input)
+    if identifier == '$':
+        return parseBulkString(input)
+    if identifier == '*':
+        return parseArray(input)
+    if identifier == ':':
+        return parseInteger(input)
+
 def parseString(input):
     if input[0] != '+' and input[-2:] != CRLF:
         raise ValueError("Incorrect format for type: String")
@@ -12,3 +25,83 @@ def parseError(input):
         raise ValueError("Incorrect format for type: Error")
     return input[1:-2]
 
+def parseInteger(input):
+    if input[0] != ':' and input[-2:] != CRLF:
+        raise ValueError("Incorrect format for type: Integer")
+    sign = 1
+    if input[1] == '-' or input[1] == '+':
+        sign = -1 if input[1] == '-' else 1 
+    return sign * int(input[1:-2])
+
+def deriveLength(input, start):
+    lengthNum = ""
+    endOfLenIndex = start
+    for i in range(start, len(input)):
+        if input[i].isdigit():
+            lengthNum += input[i]
+        else:
+            endOfLenIndex = i
+            break
+
+    return (int(lengthNum), endOfLenIndex)
+
+def parseBulkString(input):
+    if input[0] != '$' and input[-2:] != CRLF:
+        raise ValueError("Incorrect format for type: Bulk String")
+
+    # null bulk string
+    if input == '$-1\r\n':
+        return None
+    if input == '$0\r\n\r\n':
+        return ""
+    lengthNum, endOfLenIndex = deriveLength(input, 1)
+
+    if len(CRLF) + endOfLenIndex + lengthNum + len(CRLF) != len(input):
+        raise ValueError("Incorrect length for type: Bulk String")
+    if input[endOfLenIndex: endOfLenIndex+2] != CRLF:
+        raise ValueError("Missing ending CRLF for type: Bulk String")
+
+    bulkString = input[endOfLenIndex+2: -2]
+    return bulkString
+
+def findEnd(input):
+    if input[0] in "+-:" or input == '$-1\r\n':
+        return input.find(CRLF) + 1
+    if input[0] == '$':
+        if input.startswith("$0\r\n\r\n"):
+            return 5
+        firstCRLFLoc = input.find(CRLF) + 1
+        return firstCRLFLoc + 2 + input[firstCRLFLoc + 2: ].find(CRLF) + 1
+
+def parseArray(input):
+    if input[0] != '*' and input[-2:] != CRLF:
+        raise ValueError("Incorrect format for type: Array")
+    numElements, endOfLenIndex = deriveLength(input, 1)
+
+    # parse out number of elements using split with \r\n as the delimiter
+    cleanedInput = input[endOfLenIndex:]
+    outputArr = []
+    index = 0 
+    commands = ('+', '*', '-', ':', '$')
+    while index < len(cleanedInput) and numElements > 0: 
+        currInput = cleanedInput[index]
+        if cleanedInput[index: index+2] == CRLF:
+            index = index + 2
+            continue
+        if currInput in commands:
+            element = ""
+            if currInput == '*':
+                element, nextIndex = parseArray(cleanedInput[index:])
+                index += nextIndex
+            else:
+                endOfElem = findEnd(cleanedInput[index:])
+                element = parse(cleanedInput[index:index + endOfElem + 1])
+                index += endOfElem + 1
+            numElements -= 1
+            outputArr.append(element)
+        
+    return outputArr, index
+
+print(parseArray("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"))
+print(parseArray("*2\r\n$12\r\nabcde673jelz\r\n:3\r\n"))
+print(parseArray("*3\r\n:1\r\n*2\r\n+foo\r\n+bar\r\n*1\r\n*2\r\n:10\r\n:20\r\n"))
--- tests/RESP_test.py
+++ tests/RESP_test.py
@@ -1,5 +1,5 @@
 import pytest
-from RESP import parseString, parseError
+from RESP import parseString, parseError, parseInteger, parseArray, parseBulkString, findEnd
 
 @pytest.mark.parametrize("raw, expected", [
     ("+OK\r\n", "OK"),
@@ -18,3 +18,76 @@ def test_parse_string_valid_simple_strings(raw, expected):
 ])
 def test_parseError(raw, expected):
     assert parseError(raw) == expected
+
+@pytest.mark.parametrize("raw, expected", [
+    (":123\r\n", 123),
+    (":-123\r\n", 123), 
+])
+def test_parseInteger(raw, expected):
+    assert parseInteger(raw) == expected
+
+@pytest.mark.parametrize("raw", [":-123a\r\n", ":zk\r\n", ":124.58\r\n"])
+def test_parseIntegerError(raw):
+    with pytest.raises(ValueError):
+        parseInteger(raw)
+
+@pytest.mark.parametrize("raw, expected", [
+    ("$12\r\nabcde673jelz\r\n", "abcde673jelz"),
+    ("$3\r\nabc\r\n", "abc"),
+    ("$0\r\n\r\n", ""),
+])
+def test_parseBulkString(raw, expected):
+    assert parseBulkString(raw) == expected
+    
+@pytest.mark.parametrize("raw", ["$2\r\na\r\n", "$3\r\n\r", "4\r\nabcdef\r\n"])
+def test_parseBulkStringError(raw):
+    with pytest.raises(ValueError):
+        parseBulkString(raw)
+
+
+@pytest.mark.parametrize("raw, expected", [
+    ("*2\r\n$12\r\nabcde673jelz\r\n:3\r\n", ["abcde673jelz", 3]),
+    ("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n", ["hello", "world"]),
+    ("*3\r\n:1\r\n*2\r\n+foo\r\n+bar\r\n*1\r\n*2\r\n:10\r\n:20\r\n", [1,["foo", "bar"],[[10, 20]]]),
+    ("*0\r\n", []),
+    ("*3\r\n:1\r\n:2\r\n:3\r\n", [1, 2, 3]),
+    ("*2\r\n$0\r\n\r\n+OK\r\n", ["", "OK"]),
+    ("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n", ["hello", "world"]),
+    ("*3\r\n:1\r\n*2\r\n+foo\r\n+bar\r\n*1\r\n*2\r\n:10\r\n:20\r\n", [1, ["foo", "bar"], [[10, 20]]]),
+    ("*4\r\n+OK\r\n-ERR bad thing\r\n:123\r\n$5\r\nhello\r\n", ["OK", "ERR bad thing", 123, "hello"]),
+    ("*2\r\n*0\r\n*0\r\n", [[], []]),
+    ("*3\r\n*1\r\n+one\r\n*2\r\n:2\r\n:3\r\n*3\r\n+four\r\n+five\r\n+six\r\n", [["one"], [2, 3], ["four", "five", "six"]]),
+    ("*1\r\n*1\r\n*1\r\n*1\r\n+deep\r\n", [[[[ "deep" ]]]]),
+    ("*2\r\n$11\r\nhello world\r\n*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", ["hello world", ["foo", "bar"]]),
+    ("*5\r\n:0\r\n:-1\r\n:999999\r\n$0\r\n\r\n+END\r\n", [0, -1, 999999, "", "END"]),
+    ("*3\r\n*2\r\n$3\r\nfoo\r\n:10\r\n*2\r\n$3\r\nbar\r\n:20\r\n*2\r\n$3\r\nbaz\r\n:30\r\n", [["foo", 10], ["bar", 20], ["baz", 30]]),
+    ("*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n*2\r\n+yes\r\n+no\r\n*1\r\n$4\r\ntest\r\n",[[1, 2, 3], [["yes", "no"], ["test"]]]),
+    ])
+
+def test_parseArray(raw, expected):
+    assert parseArray(raw)[0] == expected
+
+@pytest.mark.parametrize("raw, expected", [
+    ("$12\r\nabcde673jelz\r\n", 18),
+    ("$3\r\nabc\r\n", 8),
+    (":3\r\n", 3),
+    (":34\r\n", 4), 
+])
+def test_findEnd(raw, expected):
+    assert findEnd(raw) == expected
+    
+
+'''
+handling array
+
+- it can contain any type, thinking of how to parse, or chop since simple crlf delimiter wont work
+- was thinking i can only use crlf as a delimiter conditionally
+    - We can use the crlf if isn't in front of a new element (identified by *, +, $, -)
+- the only problem is that what if we have a nested bulk array?
+- thinking of having a parsing loop which determines if the types are any other type then we
+  can separate, otherwise if bulk array recursively call this bulk array function on it
+    - the issue when calling the inner function then how does that one know to end?
+    - i think the array parser can basically just stop at it's total elements?
+    - so like *3 would do the first 3 and return
+
+'''
